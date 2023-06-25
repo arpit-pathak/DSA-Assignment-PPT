f63b91ffe94c4813da8800b17bbcd90982160fa2
code-today <blogarpit21@gmail.com>
2023-06-25 23:33:44 +0530

    added assignment 13

---
+++ Assignment 13 - Linked List/Solution_1.js
@@ -0,0 +1,13 @@
+// 💡 **Question 1**
+
+// Given two linked list of the same size, the task is to create a new linked list using those linked lists. The condition is that the greater node among both linked list will be added to the new linked list.
+
+// **Examples:**
+
+// Input: list1 = 5->2->3->8
+// list2 = 1->7->4->5
+// Output: New list = 5->7->4->8
+
+// Input:list1 = 2->8->9->3
+// list2 = 5->3->6->4
+// Output: New list = 5->8->9->4
---
+++ Assignment 13 - Linked List/Solution_6.js
@@ -0,0 +1,11 @@
+// 💡 **Question 6**
+
+// Given two sorted linked lists consisting of **N** and **M** nodes respectively. The task is to merge both of the lists (in place) and return the head of the merged list.
+
+// **Examples:**
+
+// Input: a: 5->10->15, b: 2->3->20
+// Output: 2->3->5->10->15->20
+
+// Input: a: 1->1, b: 2->4
+// Output: 1->1->2->4
