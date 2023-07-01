8c2552c00d8f337efcb5b67ba6044abe4fd00e4b
code-today <blogarpit21@gmail.com>
2023-07-01 23:46:14 +0530

    added assignment 21 tree

---
+++ Assignment 21 - Tree/Solution_4.js
@@ -0,0 +1,31 @@
+// 💡 Question-4:
+
+// Write a program to connect nodes at the same level.
+
+// Input:
+
+//         1
+
+//       /   \
+
+//     2      3
+
+//   /   \   /   \
+
+// 4     5 6    7
+
+// Output:
+
+// 1 → -1
+
+// 2 → 3
+
+// 3 → -1
+
+// 4 → 5
+
+// 5 → 6
+
+// 6 → 7
+
+// 7 → -1
