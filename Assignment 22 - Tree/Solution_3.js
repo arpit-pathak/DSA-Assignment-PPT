7c85524aaa5a64db5e9f5dfaba3e7b6fc70a47f9
code-today <blogarpit21@gmail.com>
2023-07-03 23:20:00 +0530

    added solution 3,4 in assignment 22

--- Assignment 22 - Tree/Solution_3.js
+++ Assignment 22 - Tree/Solution_3.js
@@ -19,3 +19,61 @@
 //   6->3->5->7
 //   6->3->5->4
 //   6->5>4
+
+class TreeNode {
+  constructor(value) {
+    this.val = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const printRootToLeafPaths = (root) => {
+  if (root === null) {
+    return;
+  }
+
+  // Stack to store the nodes during DFS traversal
+  const nodeStack = [];
+  // Stack to store the paths from the root to each node
+  const pathStack = [];
+
+  // Push the root node and its path onto the stacks
+  nodeStack.push(root);
+  pathStack.push(root.val.toString());
+
+  while (nodeStack.length > 0) {
+    const node = nodeStack.pop();
+    const path = pathStack.pop();
+
+    // Check if the node is a leaf node
+    if (node.left === null && node.right === null) {
+      console.log(path); // Print the root-to-leaf path
+    }
+
+    // Process the left child
+    if (node.left !== null) {
+      nodeStack.push(node.left);
+      pathStack.push(path + "->" + node.left.val);
+    }
+
+    // Process the right child
+    if (node.right !== null) {
+      nodeStack.push(node.right);
+      pathStack.push(path + "->" + node.right.val);
+    }
+  }
+};
+
+// Create the binary tree
+const root = new TreeNode(6);
+root.left = new TreeNode(3);
+root.right = new TreeNode(5);
+root.left.left = new TreeNode(2);
+root.left.right = new TreeNode(5);
+root.right.right = new TreeNode(4);
+root.left.right.left = new TreeNode(7);
+root.left.right.right = new TreeNode(4);
+
+// Print all root-to-leaf paths
+printRootToLeafPaths(root);
--- Assignment 22 - Tree/Solution_4.js
+++ Assignment 22 - Tree/Solution_4.js
@@ -31,3 +31,57 @@
 // Output :
 
 // No
+
+const areTraversalsSame = (inorder, preorder, postorder) => {
+  if (inorder.length === 0 || preorder.length === 0 || postorder.length === 0) {
+    return false;
+  }
+
+  if (inorder.length === 1 && preorder.length === 1 && postorder.length === 1) {
+    return (
+      inorder[0] === preorder[0] &&
+      preorder[0] === postorder[0] &&
+      postorder[0] === inorder[0]
+    );
+  }
+
+  if (
+    inorder.length !== preorder.length ||
+    preorder.length !== postorder.length
+  ) {
+    return false;
+  }
+
+  const rootValue = preorder[0];
+  const rootIndex = inorder.indexOf(rootValue);
+
+  const leftInorder = inorder.slice(0, rootIndex);
+  const rightInorder = inorder.slice(rootIndex + 1);
+
+  const leftPreorder = preorder.slice(1, rootIndex + 1);
+  const rightPreorder = preorder.slice(rootIndex + 1);
+
+  const leftPostorder = postorder.slice(0, rootIndex);
+  const rightPostorder = postorder.slice(rootIndex, -1);
+
+  const leftSubtreeSame = areTraversalsSame(
+    leftInorder,
+    leftPreorder,
+    leftPostorder
+  );
+
+  const rightSubtreeSame = areTraversalsSame(
+    rightInorder,
+    rightPreorder,
+    rightPostorder
+  );
+
+  return leftSubtreeSame && rightSubtreeSame;
+};
+
+// Example usage:
+const inorder = [4, 2, 5, 1, 3];
+const preorder = [1, 2, 4, 5, 3];
+const postorder = [4, 5, 2, 3, 1];
+
+console.log(areTraversalsSame(inorder, preorder, postorder)); // Output: true
